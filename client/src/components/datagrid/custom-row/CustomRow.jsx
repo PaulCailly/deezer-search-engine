915bde03b935d1a1c2e100f4d5aebe39c96725ed
Paul Cailly <hello@paulcailly.fr>
2019-04-22 18:35:20 +0200

    fix warning

--- client/src/components/datagrid/custom-row/CustomRow.jsx
+++ client/src/components/datagrid/custom-row/CustomRow.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from "react";
 import TableRow from "@material-ui/core/TableRow";
 
-const CustomRow = ({ dataLength, inputValue, doFetch, ...props }) => {
+const CustomRow = ({
+  dataLength,
+  inputValue,
+  doFetch,
+  children,
+  row,
+  tableRow
+}) => {
   useEffect(() => {
-    if (dataLength - 1 === props.tableRow.rowId) {
+    if (dataLength - 1 === tableRow.rowId) {
       doFetch(inputValue, dataLength);
     }
   }, []);
-  return <TableRow {...props} />;
+
+  return <TableRow children={children} row={row} />;
 };
 
 export default CustomRow;
